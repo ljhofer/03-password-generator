81f8bdf820bd0d2e176350cddc2dbb9fdea2b14c
Laura Hofer <ljhofer@gmail.com>
2021-09-17 17:06:02 -0500

    Created global variables, window confirms, and window prompts.

---
+++ script.js
@@ -0,0 +1,60 @@
+// Define global variables
+var numberOfCharacters = ""
+var allowSpecialCharacters = ""
+var allowNumbers = ""
+var allowLowerCase = ""
+var allowUpperCase = ""
+
+
+// function askHowManyCharacters() {
+  var answer = window.prompt("How any character would you like in the password? Choose a number between 8 and 128.");
+    console.log(answer)
+
+// }
+
+
+// function askSpecialCharacters() {
+  var answer = window.confirm("Would you like to include special characters?");
+  console.log(answer)
+// }
+
+
+// function askNumbers() {
+  var answer = window.confirm("Would you like to include numbers?");
+  console.log(answer)
+// }
+
+
+// function askLowerCase() {
+  var answer = window.confirm("Would you like to include lower case letters?");
+  console.log(answer)
+// }
+
+
+// function askUpperCase() {
+  var answer = window.confirm("Would you like to include uppper case letters?");
+  console.log(answer)
+// }
+
+
+
+
+
+
+
+
+
+// Assignment Code
+// var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+// function writePassword() {
+//   var password = generatePassword();
+//   var passwordText = document.querySelector("#password");
+
+//   passwordText.value = password;
+
+// }
+
+// Add event listener to generate button
+// generateBtn.addEventListener("click", writePassword);
