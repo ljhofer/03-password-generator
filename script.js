1761d0eaa269d7be69219d1d17d1f9dc1f85ba7a
Laura Hofer <ljhofer@gmail.com>
2021-09-17 18:00:45 -0500

    Defined password character variable

--- script.js
+++ script.js
@@ -5,35 +5,41 @@ var allowNumbers = ""
 var allowLowerCase = ""
 var allowUpperCase = ""
 
+var specialCharacters = [".", "!", "?", "#", "$", "%", "&", "@", "^"];
+var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9,];
+var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
 
 // function askHowManyCharacters() {
-  var answer = window.prompt("How any character would you like in the password? Choose a number between 8 and 128.");
-    console.log(answer)
+  var numCharacters = window.prompt("How any character would you like in the password? Choose a number between 8 and 128.");
+  numberOfCharacters = parseInt(numberOfCharacters);  
+  console.log(numberOfCharacters);
 
 // }
 
 
 // function askSpecialCharacters() {
-  var answer = window.confirm("Would you like to include special characters?");
-  console.log(answer)
+  allowSpecialCharacters = window.confirm("Would you like to include special characters?");
+  console.log(allowSpecialCharacters);
 // }
 
 
 // function askNumbers() {
-  var answer = window.confirm("Would you like to include numbers?");
-  console.log(answer)
+  allowNumbers = window.confirm("Would you like to include numbers?");
+  console.log(allowNumbers);
 // }
 
 
 // function askLowerCase() {
-  var answer = window.confirm("Would you like to include lower case letters?");
-  console.log(answer)
+ allowLowerCase = window.confirm("Would you like to include lower case letters?");
+  console.log(allowLowerCase);
 // }
 
 
 // function askUpperCase() {
-  var answer = window.confirm("Would you like to include uppper case letters?");
-  console.log(answer)
+  allowUpperCase = window.confirm("Would you like to include uppper case letters?");
+  console.log(allowUpperCase);
 // }
 
 
