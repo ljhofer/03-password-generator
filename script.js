280e5261e40c76b2b358d750de3f37780cf8df0e
Laura Hofer <ljhofer@gmail.com>
2021-09-19 10:37:02 -0500

    Fixed bug in random generation, created final password variable, and fixed concatenation bug

--- script.js
+++ script.js
@@ -5,19 +5,20 @@ var allowNumbers = ""
 var allowLowerCase = ""
 var allowUpperCase = ""
 
-var allowedCharacters = ""
-var password = ""
+var allowedCharacters = []
+var passwordCharacters = ""
 
 var specialCharacters = [".", "!", "?", "#", "$", "%", "&", "@", "^"];
-var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9,];
+var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
 
 
 // function askHowManyCharacters() {
   var numCharacters = window.prompt("How any character would you like in the password? Choose a number between 8 and 128.");
   numberOfCharacters = parseInt(numCharacters);  
-  console.log(numberOfCharacters);
+  // console.log(numberOfCharacters);
 
   // If number isn't between 8 and 128:
 if (numberOfCharacters < 8 || numberOfCharacters > 128)
@@ -71,16 +72,14 @@ if (!allowSpecialCharacters && !allowNumbers && !allowLowerCase && !allowUpperCa
   console.log("You haven't chosen any characters. Please select again");
 }
 
+console.log(allowedCharacters.length)
 //For loop for character selection
 for (var i = 0; i < numberOfCharacters; i++) {
-   password = allowedCharacters[Math.floor(Math.random() * numberOfCharacters)];
-   console.log(password)
-  
-  // var sentence = "Your password is "+ password + ".";
-  // return sentence;
+   passwordCharacters+= allowedCharacters[Math.floor(Math.random() * allowedCharacters.length)];
+   
 }
     
-
+console.log(passwordCharacters)
 
 
 
